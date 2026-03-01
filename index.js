4f1f471e0884f4189d4d94b4f3f38d7057ca4c2f
Alan <alan4tomaz8@gmail.com>
2026-02-28 22:46:19 -0300

    Update

--- index.js
+++ index.js
@@ -8,17 +8,57 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(express.json());
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+
+let urls = [];
+
+app.get("/api/shorturl/:shorturl", (req, res) => {
+
+  console.log("Acessando URL encurtada:", req.params.shorturl);
+
+  const shortUrlNumber = Number(req.params.shorturl);
+
+  const foundUrl = readUrls(shortUrlNumber);
+
+  if (foundUrl) {
+    return res.status(404).json({ error: "URL não encontrada" });
+  }
+
+  res.redirect(foundUrl);
 });
 
-app.listen(port, function() {
+app.post('/api/shorturl', function (req, res) {
+  const { original_url } = req.body;
+
+  const short_url = urls.length;
+
+  const newUrl = {
+    original_url,
+    short_url
+  };
+
+  saveUrls(original_url);
+
+  res.json(newUrl);
+});
+
+function saveUrls(url) {
+  urls.push(url)
+}
+
+function readUrls(index) {
+
+  return urls[index];
+}
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
