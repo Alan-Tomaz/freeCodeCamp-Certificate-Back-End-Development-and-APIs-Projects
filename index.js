a7659bd08731cdd9b324508eff51277f399e8fa3
Alan <alan4tomaz8@gmail.com>
2026-03-01 14:19:28 -0300

    Upload

--- index.js
+++ index.js
@@ -1,42 +1,27 @@
+require('dotenv').config()
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
 const multer = require("multer");
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-const upload = multer({
-  storage: multer.memoryStorage()
-});
+/* app.use('/public', express.static(process.cwd() + '/public'));
+ */
+const upload = multer();
 
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/', function (req, res) {
-  if (!req.file) {
-    return res.status(400).json({ error: "No file uploaded" });
-  }
-
-  console.log(req.file)
-  res.json({
-    name: req.file.originalname,
-    type: req.file.mimetype,
-    size: req.file.size
-  });
-});
-
-
 app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  console.log(req.file)
+
   if (!req.file) {
     return res.status(400).json({ error: "No file uploaded" });
   }
 
-  console.log(req.file)
   res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
