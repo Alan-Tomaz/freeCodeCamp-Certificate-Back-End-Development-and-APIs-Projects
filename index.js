c5028044b5f4450c68131cb4d1c59d0c516e135a
Alan <alan4tomaz8@gmail.com>
2026-03-01 12:25:53 -0300

    Update

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ const port = process.env.PORT || 3000;
 const dns = require("dns");
 /* const { URL} = require("url"); */
 const urlparser = require("url");
-
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+const { MongoClient } = require('mongodb')
+const cdb = new MongoClient(process.env.URI);
+const db = cdb.db("url_service");
+const storeUrls = db.collection('urls');
 
 app.use(cors());
-
-app.use(express.urlencoded({ extended: false }));
+app.use(limiter);
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -30,15 +38,34 @@ let num = 0;
 
 app.post("/api/shorturl", async function (req, res) {
 
-  const urlRegex = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-  const url = req.body.url;
-  if (url.match(urlRegex)) {
-    num++;
-    urls.push({ short_url: num, original_url: url });
-    res.json({ original_url: url, short_url: num });
-  } else {
-    res.json({ error: 'invalid URL' });
-  }
+  const urlString = req.body.url;
+  const lookDns = dns.lookup(urlparser.parse(urlString).hostname, async (req, validAddress) => {
+    if (!validAddress) {
+      res.json({ error: "Invalid URL" })
+    } else {
+      const countUrls = await storeUrls.countDocuments({});
+      const urlStore = {
+        urlString,
+        short_url: countUrls
+      }
+      const respond = await storeUrls.insertOne(urlStore);
+      console.log(respond);
+      res.json({
+        original_url: urlString,
+        short_url: countUrls
+      });
+    }
+  })
+
+  /*  const urlRegex = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+   const url = req.body.url;
+   if (url.match(urlRegex)) {
+     num++;
+     urls.push({ short_url: num, original_url: url });
+     res.json({ original_url: url, short_url: num });
+   } else {
+     res.json({ error: 'invalid URL' });
+   } */
 
   /*   const urlString = req.body.url;
     const lookDns = dns.lookup(urlparser.parse(urlString).hostname, async (req, validAddress) => {
@@ -86,14 +113,17 @@ app.post("/api/shorturl", async function (req, res) {
    */
 });
 
-app.get('/api/shorturl/:short_url', (req, res) => {
-  const short = parseInt(req.params.short_url);
-  const url = urls.find(data => data.short_url === short);
-  if (url) {
-    res.redirect(url.original_url);
-  } else {
-    res.json({ error: 'invalid URL' });
-  }
+app.get('/api/shorturl/:short_url', async (req, res) => {
+  const shorturl = req.params.short_url;
+  const urlStore = await storeUrls.findOne({ short_url: +shorturl })
+  res.redirect(urlStore.urlString);
+  /*   const short = parseInt(req.params.short_url);
+    const url = urls.find(data => data.short_url === short);
+    if (url) {
+      res.redirect(url.original_url);
+    } else {
+      res.json({ error: 'invalid URL' });
+    } */
 });
 
 app.listen(port, function () {
