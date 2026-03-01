9fe5f5677f55a7a53cff75779d52b14a02448adc
Alan <alan4tomaz8@gmail.com>
2026-03-01 14:11:02 -0300

    Upload

--- index.js
+++ index.js
@@ -17,6 +17,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/', function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
+  console.log(req.file)
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
+
 
 app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
   if (!req.file) {
