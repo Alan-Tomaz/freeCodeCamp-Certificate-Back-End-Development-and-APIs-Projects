4d1bcb0a5d5cfb554d667d917fae1d6574b184d4
Alan <alan4tomaz8@gmail.com>
2026-03-01 02:08:44 -0300

    Update

--- index.js
+++ index.js
@@ -25,28 +25,40 @@ app.get('/', function (req, res) {
 // Your first API endpoint
 
 let urls = [];
+let num = 0;
 
 
 app.post("/api/shorturl", async function (req, res) {
-  const urlString = req.body.url;
-  const lookDns = dns.lookup(urlparser.parse(urlString).hostname, async (req, validAddress) => {
-    if (!validAddress) {
-      res.json({ error: "Invalid URL" })
-    } else {
-      const short_url = urls.length + 1;
 
-      urlObj = {
-        original_url: urlString.trim(),
-        short_url
-      };
+  const urlRegex = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  const url = req.body.url;
+  if (url.match(urlRegex)) {
+    num++;
+    urls.push({ short_url: num, original_url: url });
+    res.json({ original_url: url, short_url: num });
+  } else {
+    res.json({ error: 'invalid URL' });
+  }
 
-      urls.push(urlObj);
-
-
-      console.log("URL OBJECT:", urlObj)
-      res.json(urlObj);
-    }
-  })
+  /*   const urlString = req.body.url;
+    const lookDns = dns.lookup(urlparser.parse(urlString).hostname, async (req, validAddress) => {
+      if (!validAddress) {
+        res.json({ error: "Invalid URL" })
+      } else {
+        const short_url = urls.length + 1;
+  
+        urlObj = {
+          original_url: urlString.trim(),
+          short_url
+        };
+  
+        urls.push(urlObj);
+  
+  
+        console.log("URL OBJECT:", urlObj)
+        res.json(urlObj);
+      }
+    }) */
 
   /*   const { url } = req.body;
     console.log("URL SALVA:", url)
@@ -74,15 +86,14 @@ app.post("/api/shorturl", async function (req, res) {
    */
 });
 
-app.get("/api/shorturl/:short_url", async (req, res) => {
-  console.log("PARAMS:", req.params)
-
-  const shortUrlNumber = Number(req.params.short_url);
-
-  const found = urls.find(u => u.short_url === shortUrlNumber);
-
-  console.log("URL ENCONTRADA:", found)
-  res.redirect(found.original_url);
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const short = parseInt(req.params.short_url);
+  const url = urls.find(data => data.short_url === short);
+  if (url) {
+    res.redirect(url.original_url);
+  } else {
+    res.json({ error: 'invalid URL' });
+  }
 });
 
 app.listen(port, function () {
