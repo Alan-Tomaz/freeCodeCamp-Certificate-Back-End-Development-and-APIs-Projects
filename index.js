743e799d6e4788f876d4f9b35e3b0205ef8b548a
Alan <alan4tomaz8@gmail.com>
2026-02-28 21:50:20 -0300

    Update

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get("/api/:date", function (req, res) {
   console.log(req.params.date);
   if (!isNaN(req.params.date)) {
     const date = new Date(parseInt(req.params.date));
-    res.json({ unix: Date.now(date), utc: new Date(date).toUTCString() });
+    res.json({ unix: date.getTime(), utc: new Date(date).toUTCString() });
   }
   else {
     res.json({ error: "Invalid Date" });
